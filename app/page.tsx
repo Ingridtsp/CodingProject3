b919d1183d21f9b6b85d55b8bd5cff59843f8659
Ingridtsp <ingridnicole@usf.edu>
2025-10-26 14:29:20 -0400

    Change Counter initialCount and initialStep values

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center gap-8">
       <Counter />
-      <Counter initialCount={10} initialStep={1} />
+      <Counter initialCount={5} initialStep={2} />
     </main>
   );
 }
